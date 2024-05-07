e3ae367a0f80478d54d66cf905c317aed0e5c891
mouna jmai <mouna.jmai@gmail.com>
2024-05-07 11:37:48 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+  extend: {
+  colors: {
+  'my-blue': '#007bff',
+  },
+  },
+  },
+  variants: {},
+  plugins: [],
+  }
+  
